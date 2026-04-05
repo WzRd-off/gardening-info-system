ff0df348a81b40b0599561dcea7ba0c3e418b144
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-04-05 18:44:24 +0300

    add new routes + css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainPage from './pages/Main.jsx';
 import AuthPage from './pages/Auth.jsx';
+import ContactsPage from './pages/Contacts.jsx';
+import ProfilePage from './pages/Profile.jsx';
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>  
     </>
