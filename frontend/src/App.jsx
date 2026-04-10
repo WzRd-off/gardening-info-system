710d761d56b7c587e0e1f9101682093094d72edc
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-04-10 17:42:50 +0300

    finish routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainPage from './pages/Main.jsx';
 import AuthPage from './pages/Auth.jsx';
-import ContactsPage from './pages/Contacts.jsx';
 import ProfilePage from './pages/Profile.jsx';
 import ManagerPage from './pages/Manager.jsx';
+import ContactsPage from './pages/Contacts.jsx';
+import CreateOrderPage from './pages/CreateOrder.jsx';
+import TeamPage from './pages/Team.jsx';
 
 export default function App() {
   return (
@@ -17,6 +19,8 @@ export default function App() {
         <Route path="/contacts" element={<ContactsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/manager" element={<ManagerPage />} />
+        <Route path="/services" element={<CreateOrderPage />} />
+        <Route path="/team" element={<TeamPage />} />
       </Routes>
     </Router>  
     </>
