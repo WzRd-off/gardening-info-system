aac387c305e43d3406d89a201a7dd3a8ac6c87b6
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-04-10 17:57:44 +0300

    add image to staticfiles

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, profile, manager, team, order
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+IMAGES_DIR = os.path.join(os.path.dirname(BASE_DIR), "images")
 
 app = FastAPI()
 
@@ -18,3 +24,5 @@ app.include_router(profile.router)
 app.include_router(manager.router)
 app.include_router(team.router)
 app.include_router(order.router)
+
+app.mount("/images", StaticFiles(directory=IMAGES_DIR), name='images')
